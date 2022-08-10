d90f85f7280a5830f7598237eb00baf96dce10cb
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-08-10 12:07:03 +0100

    Add mmarkup to index.html file
    Add empty UL as To Do List placeholer
    
    Add array of objects of to do tasks
    
    Loop through array and render dynamically on the DOM
    
    Add project style into style.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import _ from 'lodash';
 import './style.css';
 
-// function component() {
-//   const element = document.createElement('div');
+const todos = [
+    {
+        description: 'Wash Cloth',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Watch Netflix',
+        completed: false,
+        index: 4,
+    },
+    {
+        description: 'Write Code',
+        completed: true,
+        index: 2,
+    },
+    {
+        description: 'Read',
+        completed: false,
+        index: 3,
+    },
+    {
+        description: 'Debug Code',
+        completed: false,
+        index: 1,
+    },
+]
 
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const listContainer = document.querySelector('.lists');
+todos.sort((a, b) => a.index - b.index);
 
-//   return element;
-// }
-
-// document.body.appendChild(component());
+todos.forEach((todo, index) => {
+    const list = document.createElement('li');
+    list.innerHTML = `
+        <div>
+            <input type="checkbox" name="" class="task-complete check ${todo.completed ? 'hide' : ''}">
+            <i class="fa fa-check list-drag-btn check check-icon ${!todo.completed ? 'hide' : ''}"></i>
+            <p ${todo.completed ? 'class="completed"' : ''}>${todo.description}</p>
+        </div>
+        <i class="fa fa-ellipsis-vertical list-drag-btn"></i>
+    `;
+    listContainer.appendChild(list);
+})
