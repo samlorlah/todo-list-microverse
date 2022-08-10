4c05544ef64f0c2a8c4d65ec5247ae56aab98e74
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-08-10 12:34:55 +0100

    Correct Linter Errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 
 const todos = [
@@ -32,21 +31,15 @@ const todos = [
 const listContainer = document.querySelector('.lists');
 todos.sort((a, b) => a.index - b.index);
 
-todos.forEach((todo, index) => {
+todos.forEach((todo) => {
   const list = document.createElement('li');
   list.innerHTML = `
-        <div>
-            <input type="checkbox" name="" class="task-complete check ${
-              todo.completed ? 'hide' : ''
-            }">
-            <i class="fa fa-check list-drag-btn check check-icon ${
-              !todo.completed ? 'hide' : ''
-            }"></i>
-            <p ${todo.completed ? 'class="completed"' : ''}>${
-    todo.description
-  }</p>
-        </div>
-        <i class="fa fa-ellipsis-vertical list-drag-btn"></i>
+      <div>
+        <input type="checkbox" name="" class="task-complete check ${todo.completed ? 'hide' : ''}">
+        <i class="fa fa-check list-drag-btn check check-icon ${!todo.completed ? 'hide' : ''}"></i>
+        <p ${todo.completed ? 'class="completed"' : ''}>${todo.description}</p>
+      </div>
+      <i class="fa fa-ellipsis-vertical list-drag-btn"></i>
     `;
   listContainer.appendChild(list);
 });
