240c21d0337d7d1a49f7a4b4e798620d539cac31
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-08-10 12:11:30 +0100

    Format Document for Linters Errors

--- src/index.js
+++ src/index.js
@@ -2,45 +2,51 @@ import _ from 'lodash';
 import './style.css';
 
 const todos = [
-    {
-        description: 'Wash Cloth',
-        completed: false,
-        index: 0,
-    },
-    {
-        description: 'Watch Netflix',
-        completed: false,
-        index: 4,
-    },
-    {
-        description: 'Write Code',
-        completed: true,
-        index: 2,
-    },
-    {
-        description: 'Read',
-        completed: false,
-        index: 3,
-    },
-    {
-        description: 'Debug Code',
-        completed: false,
-        index: 1,
-    },
-]
+  {
+    description: 'Wash Cloth',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Watch Netflix',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Write Code',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Read',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Debug Code',
+    completed: false,
+    index: 1,
+  },
+];
 
 const listContainer = document.querySelector('.lists');
 todos.sort((a, b) => a.index - b.index);
 
 todos.forEach((todo, index) => {
-    const list = document.createElement('li');
-    list.innerHTML = `
+  const list = document.createElement('li');
+  list.innerHTML = `
         <div>
-            <input type="checkbox" name="" class="task-complete check ${todo.completed ? 'hide' : ''}">
-            <i class="fa fa-check list-drag-btn check check-icon ${!todo.completed ? 'hide' : ''}"></i>
-            <p ${todo.completed ? 'class="completed"' : ''}>${todo.description}</p>
+            <input type="checkbox" name="" class="task-complete check ${
+              todo.completed ? 'hide' : ''
+            }">
+            <i class="fa fa-check list-drag-btn check check-icon ${
+              !todo.completed ? 'hide' : ''
+            }"></i>
+            <p ${todo.completed ? 'class="completed"' : ''}>${
+    todo.description
+  }</p>
         </div>
         <i class="fa fa-ellipsis-vertical list-drag-btn"></i>
     `;
-    listContainer.appendChild(list);
-})
+  listContainer.appendChild(list);
+});
